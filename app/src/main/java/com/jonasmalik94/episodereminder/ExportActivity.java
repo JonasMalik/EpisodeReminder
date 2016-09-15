0a4f81af78d5bf0f7270682480c33c2bb2f9b783
JonasMalik <jonas14550@gmail.com>
2016-09-15 12:37:10 +0200

    now it is possible to delete and import a backup of the DB

--- app/src/main/java/com/jonasmalik94/episodereminder/ExportActivity.java
+++ app/src/main/java/com/jonasmalik94/episodereminder/ExportActivity.java
@@ -25,8 +25,13 @@ public class ExportActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export);
 
+        ActivityCompat.requestPermissions(ExportActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                             Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                1);
+
         Button exportDB = (Button) findViewById(R.id.exportDB);
-        Button importDB = (Button) findViewById(R.id.importDB);
+        final Button importDB = (Button) findViewById(R.id.importDB);
         Button deleteDB = (Button) findViewById(R.id.deleteDB);
 
         exportDB.setOnClickListener(new View.OnClickListener() {
@@ -35,14 +40,23 @@ public class ExportActivity extends Activity {
                 exportDB();
             }
         });
+        deleteDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteDB();
+            }
+        });
+        importDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                importDB();
+            }
+        });
 
 
     }
 
     private void exportDB(){
-        ActivityCompat.requestPermissions(ExportActivity.this,
-                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                1);
 
         String SAMPLE_DB_NAME = "EpisodeReminder";
         File sd = Environment.getExternalStorageDirectory() ;
@@ -60,6 +74,37 @@ public class ExportActivity extends Activity {
             source.close();
             destination.close();
             Toast.makeText(this, "DB Exported!", Toast.LENGTH_LONG).show();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteDB() {
+        boolean result = this.deleteDatabase("EpisodeReminder");
+        if (result == true) {
+            Toast.makeText(this, "DB Deleted!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void importDB(){
+
+        this.deleteDatabase("EpisodeReminder");
+        String SAMPLE_DB_NAME = "EpisodeReminder";
+        File sd = Environment.getExternalStorageDirectory() ;
+        File data = Environment.getDataDirectory();
+        FileChannel source=null;
+        FileChannel destination=null;
+        String currentDBPath = "/data/"+ "com.jonasmalik94.episodereminder" +"/databases/"+SAMPLE_DB_NAME;
+        String backupDBPath = SAMPLE_DB_NAME;
+        File currentDB = new File(data, currentDBPath);
+        File backupDB = new File(sd, backupDBPath);
+        try {
+            source = new FileInputStream(backupDB).getChannel();
+            destination = new FileOutputStream(currentDB).getChannel();
+            destination.transferFrom(source, 0, source.size());
+            source.close();
+            destination.close();
+            Toast.makeText(this, "DB har importerats!", Toast.LENGTH_LONG).show();
         } catch(IOException e) {
             e.printStackTrace();
         }
